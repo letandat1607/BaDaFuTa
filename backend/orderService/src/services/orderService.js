f56a4389c3ce27df5aa3602ba1842eb2595f8d82
letandat1607 <dat16072004@gmail.com>
2025-12-07 18:35:46 +0700

    fix: error msg validate data

--- backend/orderService/src/services/orderService.js
+++ backend/orderService/src/services/orderService.js
@@ -26,7 +26,7 @@ module.exports.createOrder = async (data, userId) => {
         // console.log("Create order service data:", data);
 
         const { value, error } = orderSchema.validate({status: "waiting", ...data}, {stripUnknown: true});
-        if (error) throw new Error(error.message);
+        if (error) throw new Error("Đơn hàng không hợp lệ");
 
         const { server_total } = await validateOrder(data);
         console.log("server_total:", server_total, "client_total:", data.total_amount);
